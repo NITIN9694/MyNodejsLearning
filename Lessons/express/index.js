a15396426834515baee5b8c3fad336896f132802
NITIN9694 <nitinjha532k@gmail.com>
2022-12-18 00:37:05 +0530

    code_pushed

--- Lessons/express/index.js
+++ Lessons/express/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+
+const publicPath = path.join(__dirname,'html');
+
+app.use(express.static(publicPath));
+
 
 app.get("/check",(req,res)=>{
     res.send("Its Working");
--- Lessons/input_command_line/index.js
+++ Lessons/input_command_line/index.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 
 const input = process.argv;
 if(input[2]=="add"){
-    fs.writeFileSync(input
-        [3],input[2]);
+    fs.writeFileSync(input[3],input[2]);
 }else if("remove"){
    fs.unlinkSync(input[3]);
 }else{
